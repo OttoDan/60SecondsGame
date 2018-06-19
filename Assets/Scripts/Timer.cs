240dac1698b8f72b91eb0962aa1412d525782001
Koala <joscha.leon.haberstock@gmail.com>
2018-06-19 12:16:49 +0200

    Add Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text TimeText;
+    public float time = 60;
+    public bool OnGame = true;
+
+    void Start()
+    {
+        OnGame = true;
+    }
+
+    void Update()
+    {
+
+        time -= Time.deltaTime;
+        
+        if (OnGame = true)
+        {
+            TimeText.text = ("" + time);
+        }
+
+        if (time < 0)
+        {
+            OnGame = false;
+        }
+
+    }
+
+
+
+
+
+}

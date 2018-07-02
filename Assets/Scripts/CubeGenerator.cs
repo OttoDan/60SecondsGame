811db288058605708c0349a592c167d135768214
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-07-02 15:34:49 +0200

    Revert "Cleaned Project"
    This reverts commit bcc0c8cc5a57b0c530738166bb9aaf0ade187af2.

---
+++ Assets/Scripts/CubeGenerator.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CubeGenerator : EditorWindow {
+
+    #region Private Fields
+
+
+
+    #endregion
+
+    #region Public Fields
+
+    public int width, height;
+
+    public float cubeWidth, cubeHeight;
+
+    public GameObject cubePrefab;
+
+
+    #endregion
+
+    #region Window Functions
+
+    [MenuItem("CubeTools/CubeGenerator")]
+    static void Init()
+    {
+        CubeGenerator window = (CubeGenerator)EditorWindow.GetWindow(typeof(CubeGenerator));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        if (cubePrefab == null)
+        {
+            EditorGUILayout.HelpBox("Please assign a cube prefab reference.", MessageType.Info);
+            // GUILayout.Label("Please assign a prefab reference.", GUI.tooltip);
+        }
+
+
+        width = EditorGUILayout.IntField("Width", width);
+        height = EditorGUILayout.IntField("Height", height);
+
+        cubePrefab = EditorGUILayout.ObjectField("Prefab", cubePrefab, typeof(GameObject), false) as GameObject;
+
+        cubeWidth = EditorGUILayout.FloatField("Cube Width", cubeWidth);
+        cubeHeight = EditorGUILayout.FloatField("Cube Height", cubeHeight);
+
+        EditorGUILayout.BeginHorizontal();
+        {
+            EditorGUI.BeginDisabledGroup(cubePrefab == null);
+            {
+                if (GUILayout.Button("Generate Cube"))
+                {
+                    GenerateCube(width, height, cubeWidth, cubeHeight);
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void GenerateCube(int width=0, int height=0, float cubeWidth = 1, float cubeHeight = 1)
+    {
+        Transform parent = new GameObject("ThaCube").transform;
+        if(width == 0)
+            width = this.width;
+        if (height == 0)
+            height = this.height;
+
+        //Debug.LogFormat("Width: {0}\nHeight: {1}", width, height);
+
+        Vector3 right = Vector3.right;
+        Vector3 forward = Vector3.forward;
+
+        Transform currentSide;
+
+        for(int i = 0; i < 6; i++)
+        {
+            currentSide = new GameObject("Side" + i).transform;
+            currentSide.parent = parent;
+
+            
+            
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    GameObject copy = Instantiate(cubePrefab, (-right * width * 0.5f) + x * right + (-forward * height * 0.5f) + y * forward, Quaternion.identity, currentSide);
+                    copy.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+                    //copy.transform.localScale = new Vector3(cubeWidth, cubeHeight, 1);
+                }
+            }
+
+            switch (i)
+            {
+                case 0:
+                    currentSide.localPosition = Vector3.up * height * 0.5f;
+                    break;
+
+                case 1:
+                    currentSide.localPosition = Vector3.forward * height * 0.5f;
+                    currentSide.localRotation = Quaternion.Euler(new Vector3(90, 0, 0));
+                    break;
+
+                case 2:
+                    currentSide.localPosition = -Vector3.up * height * 0.5f;
+                    currentSide.localRotation = Quaternion.Euler(new Vector3(180, 0, 0));
+                    break;
+
+                case 3:
+                    currentSide.localPosition = -Vector3.forward * height * 0.5f;
+                    currentSide.localRotation = Quaternion.Euler(new Vector3(270, 0, 0));
+                    break;
+
+                case 4:
+                    currentSide.localPosition = Vector3.right * height * 0.5f;
+                    currentSide.localRotation = Quaternion.Euler(new Vector3(0, 0, -90));
+                    break;
+
+                case 5:
+                    currentSide.localPosition = -Vector3.right * height * 0.5f;
+                    currentSide.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+                    break;
+
+            }
+
+
+        }
+
+        Camera.main.transform.position = Vector3.right * width - Vector3.forward * height + Vector3.up * height;
+        Camera.main.transform.LookAt(parent);
+
+        //GameObject.FindGameObjectWithTag("Player").transform.position = -Vector3.forward * height +  Vector3.up * height;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/PlayerCube.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCube : MonoBehaviour {
+    public static PlayerCube Instance;
+
+    LineRenderer lineRenderer;
+
+    List<Vector3> dashPositions = new List<Vector3>();
+    bool isPaused;
+    IEnumerator DashCoroutine;
+
+    public Canvas canvas;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        lineRenderer = GetComponent<LineRenderer>();
+        isPaused = true;
+    }
+
+    void Update()
+    {
+
+        if (isPaused)
+        {
+            Debug.Log(dashPositions.Count);
+            if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                if (dashPositions.Count == 0)
+                {
+                    lineRenderer.positionCount = 2;
+                    lineRenderer.SetPosition(0, transform.position);
+                }
+                else
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(ray, out hitInfo))
+                    {
+                        lineRenderer.positionCount = lineRenderer.positionCount + 1;
+                        lineRenderer.SetPosition(lineRenderer.positionCount - 1, hitInfo.point);
+                    }
+                }
+            }
+
+            if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, hitInfo.point);
+                }
+            }
+
+            if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    dashPositions.Add(hitInfo.point);
+                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, hitInfo.point);
+                }
+            }
+        }
+
+
+        Debug.DrawLine(Vector3.zero, transform.position, Color.red);
+    }
+
+    public void Dash()
+    {
+        if (DashCoroutine == null && dashPositions.Count > 0)
+        {
+            canvas.enabled = false;
+            isPaused = false;
+            DashCoroutine = DashRoutine();
+            StartCoroutine(DashCoroutine);
+        }
+    }
+
+    IEnumerator DashRoutine()
+    {
+        lineRenderer.positionCount = 0;
+        for (int i = 0; i < dashPositions.Count; i++)
+        {
+            float duration;
+            if (i == 0)
+                duration = Vector3.Distance(dashPositions[0], transform.position) * 0.125f;
+            else
+                duration = Vector3.Distance(dashPositions[i], dashPositions[i - 1]) * 0.125f;
+
+            Vector3 fromPos = transform.position;
+            for (float t = 0; t < duration; t += Time.deltaTime)
+            {
+                transform.position = Vector3.Lerp(fromPos, dashPositions[i], t / duration);
+                yield return null;
+            }
+        }
+        dashPositions.Clear();
+        isPaused = true;
+        canvas.enabled = true;
+        DashCoroutine = null;
+
+
+    }
+}

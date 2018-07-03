be7bcf1aacf774a159fd152d8a412f24b5406e56
Koala <joscha.leon.haberstock@gmail.com>
2018-07-03 11:34:57 +0200

    menu

---
+++ Assets/Highscore/ScoreData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ScoreData
+{
+
+    public string name = "";
+    public float highscore;
+
+    public ScoreData(string name, float highscore)
+    {
+        this.name = name;
+        this.highscore = highscore;
+    }
+}
---
+++ Assets/Scripts/Menu/VolumeSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour {
+
+	public Slider volume;
+    public AudioSource music;
+	
+	// Update is called once per frame
+	void Update () {
+
+        music.volume = volume.value;
+
+	}
+}

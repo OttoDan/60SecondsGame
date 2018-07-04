a11eb0c25d6b27e0bf2eb7c0761e5fad42f12cad
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-07-04 13:23:05 +0200

    Main menu (#2)
    * menu
    
    * new menu
    
    * Menu Animation
    
    * resolved mergeconflict(hopefully)

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+
+    public static Timer instanceTimer;
+    public Text TimeText;
+    public Text ScoreText;
+    public float time = 60;
+    public bool OnGame = true;
+    public float score;
+    public GameObject ScoreMenuPrefab;
+    
+    private void Awake()
+    {
+        if (instanceTimer == null)
+            instanceTimer = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        OnGame = true;
+
+        
+    }
+
+    void Update()
+    {
+        if (time > -1)
+        {
+            time -= Time.deltaTime;
+        }
+        
+        
+        if (Input.GetButton("Submit"))
+        {
+            score += 1;
+        }
+
+
+        if (OnGame == true)
+        {
+            TimeText.text = ("" + time);
+            ScoreText.text = ("HighScore " + score);
+            ScoreMenuPrefab.SetActive(false);
+        }
+        
+        if (OnGame == false)
+        {
+            ScoreMenuPrefab.SetActive(true);
+        }
+        
+
+        if (time < 0)
+        {
+            OnGame = false;
+            time = -1;
+
+        }
+    }
+}

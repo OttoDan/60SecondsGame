7bc2529574876ad53595c942b64a7ba0b3ddc473
MrBasenji <34125370+MrBasenji@users.noreply.github.com>
2018-07-11 08:47:11 +0200

    Enemies_not_finished
    Enemies

---
+++ Assets/Scripts/GhostlyScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostlyScript : MonoBehaviour {
+
+    #region private fields
+
+    private SkinnedMeshRenderer tempMesh;
+    private Color tempCol;
+    private float timeVisible = 10f;
+
+    IEnumerator FadeInOutCoroutine;
+
+    #endregion
+
+    #region public fields
+    
+    
+    #endregion
+    
+    // Use this for initialization
+    void Start () {
+        tempMesh = GetComponent<SkinnedMeshRenderer>();
+        StartFade();
+    }
+
+    private void Update()
+    {
+        //if (FadeInOutCoroutine == null)
+        //    StartFade();
+    }
+
+    void StartFade()
+    {
+        if (FadeInOutCoroutine == null)
+            FadeInOutCoroutine = FadeInOut();
+        else
+            StopCoroutine(FadeInOutCoroutine);
+            StartCoroutine(FadeInOutCoroutine);
+    }
+    
+IEnumerator FadeInOut(float duration = 1f)
+    {
+        //fadein
+        for(float t=0; t<duration; t+= Time.unscaledDeltaTime)
+        {
+            tempCol = tempMesh.material.color;
+            tempCol.a =  Mathf.Lerp(1, 0, t / (duration));
+            tempMesh.material.color = tempCol;
+            yield return null;
+        }
+
+        //wait
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            yield return null;
+        }
+            //fadeout
+            for (float t = 0; t < duration; t += Time.unscaledDeltaTime)
+        {
+            tempCol = tempMesh.material.color;
+            tempCol.a = Mathf.Lerp(0, 1, t / (duration));
+            tempMesh.material.color = tempCol;
+        }
+
+        yield return new WaitForSeconds(timeVisible);
+        FadeInOutCoroutine = null;
+    }
+}

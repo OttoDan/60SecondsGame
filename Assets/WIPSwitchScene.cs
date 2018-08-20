410e12be79f7119cc3cf2e006c8d50dc6b8ee22a
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-08-20 14:49:38 +0200

    Refractured Intro Video in Unity

---
+++ Assets/WIPSwitchScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WIPSwitchScene : MonoBehaviour {
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void SwitchToLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void PlayIntroSound()
+    {
+        audioSource.Play();
+    }
+}
